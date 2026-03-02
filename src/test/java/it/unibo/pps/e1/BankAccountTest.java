49bc7bb6e630a06635538f30d34898c157cce512
Alessandro Ronchi - alessandro.ronchi7@studio.unibo.it <alessandro.ronchi7@studio.unibo.it>
2026-03-02 09:38:38 +0100

    Created BankAccount interface to define public API. Modified SilverBankAccount to implement the API

--- src/test/java/it/unibo/pps/e1/BankAccountTest.java
+++ src/test/java/it/unibo/pps/e1/BankAccountTest.java
@@ -1,6 +1,5 @@
 package it.unibo.pps.e1;
 
-import it.unibo.pps.e1.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BankAccountTest {
 
+    public static final int DEPOSIT_AMOUNT = 1000;
     private BankAccount account;
 
     @BeforeEach
     void init(){
-        this.account = new BankAccount();
+        this.account = new SilverBankAccount();
     }
 
     @Test
@@ -23,21 +23,23 @@ public class BankAccountTest {
 
     @Test
     public void testCanDeposit() {
-        this.account.deposit(1000);
-        assertEquals(1000, this.account.getBalance());
+        this.account.deposit(DEPOSIT_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT, this.account.getBalance());
     }
 
     @Test
     public void testCanWithdraw() {
-        this.account.deposit(1000);
-        this.account.withdraw(200);
-        assertEquals(799, this.account.getBalance());
+        int withdraw_amount = 200;
+        this.account.deposit(DEPOSIT_AMOUNT);
+        this.account.withdraw(withdraw_amount);
+        assertEquals(DEPOSIT_AMOUNT - withdraw_amount - 1, this.account.getBalance());
     }
 
     @Test
     public void testCannotWithdrawMoreThanAvailable(){
-        this.account.deposit(1000);
-        assertThrows(IllegalStateException.class, () -> this.account.withdraw(1200));
+        int withdraw_amount = 1200;
+        this.account.deposit(DEPOSIT_AMOUNT);
+        assertThrows(IllegalStateException.class, () -> this.account.withdraw(withdraw_amount));
     }
 
 }
