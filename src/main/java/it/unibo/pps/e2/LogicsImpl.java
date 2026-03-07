3b345f110d3738c4fc57af0e63301e14eb4eb054
Alessandro Ronchi <alessandro.ronchi0018@gmail.com>
2026-03-07 13:49:06 +0100

    Modified LogicsImpl to use ChessPiece interface also for pawn

--- src/main/java/it/unibo/pps/e2/LogicsImpl.java
+++ src/main/java/it/unibo/pps/e2/LogicsImpl.java
@@ -4,33 +4,34 @@ import java.util.*;
 
 public class LogicsImpl implements Logics {
 	
-	private final Pair<Integer,Integer> pawn;
+	private final ChessPiece pawn;
 	private final ChessPiece knight;
 	private final Random random = new Random();
 	private final int size;
+	private final PieceBehaviour noMovementBehaviour = (boardSize, initial, destination) -> false;
 
     public LogicsImpl(int size){
     	this.size = size;
-        this.pawn = this.randomEmptyPosition();
+        this.pawn = new ChessPieceImpl(size, randomEmptyPosition(), noMovementBehaviour);
         this.knight = new ChessPieceImpl(size, randomEmptyPosition(), new KnightBehaviour());
     }
 
-    public LogicsImpl(int size, Pair<Integer, Integer> pawn, Pair<Integer, Integer> knightPos) {
+    public LogicsImpl(int size, Pair<Integer, Integer> pawnPosition, Pair<Integer, Integer> knightPosition) {
         this.size = size;
-        this.pawn = pawn;
-        this.knight = new ChessPieceImpl(size, knightPos, new KnightBehaviour());
+        this.pawn = new ChessPieceImpl(size, pawnPosition, noMovementBehaviour);
+        this.knight = new ChessPieceImpl(size, knightPosition, new KnightBehaviour());
     }
     
 	private Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
     	// the recursive call below prevents clash with an existing pawn
-    	return this.pawn!=null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
+    	return this.pawn.getPosition()!=null && this.pawn.getPosition().equals(pos) ? randomEmptyPosition() : pos;
     }
     
 	@Override
 	public boolean hit(int row, int col) {
 		this.knight.move(new Pair<>(row, col));
-		return this.pawn.equals(this.knight.getPosition());
+		return this.pawn.getPosition().equals(this.knight.getPosition());
 	}
 
 	@Override
@@ -40,6 +41,6 @@ public class LogicsImpl implements Logics {
 
 	@Override
 	public boolean hasPawn(int row, int col) {
-		return this.pawn.equals(new Pair<>(row,col));
+		return this.pawn.getPosition().equals(new Pair<>(row,col));
 	}
 }
